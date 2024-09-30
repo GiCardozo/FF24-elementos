a3687d65a20e75487a4d6f58c072d327c00d16bf
GIOVANNA CARDOZO CAETANO <giovanna.ccaetano@senacsp.edu.br>
2024-09-30 20:13:48 -0300

    Disparando eventos dos elementos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-// Selecionando a div pelo ID
-let div =
-document.getElementById('meuDiv');
+// // Selecionando a div pelo ID
+// let div =
+// document.getElementById('meuDiv');
 
-// Selecionando todos os parágrafos com a classe "paragrafo"
-let paragrafos = 
-document.getElementsByClassName('paragrafo');
+// // Selecionando todos os parágrafos com a classe "paragrafo"
+// let paragrafos = 
+// document.getElementsByClassName('paragrafo');
 
-// Selacionando o primeiro parágrafo com a classe "paragrafo"
-let primeiroParagrafo =
-document.querySelector('.paragrafo');
+// // Selacionando o primeiro parágrafo com a classe "paragrafo"
+// let primeiroParagrafo =
+// document.querySelector('.paragrafo');
 
-// Selecionando todos os parágrafos dentro da div
-let paragrafoDentroDiv =
-document.querySelectorAll('#meuDiv p');
+// // Selecionando todos os parágrafos dentro da div
+// let paragrafoDentroDiv =
+// document.querySelectorAll('#meuDiv p');
+
+// Funções para os eventos
+function aoClicar() {
+    alert('Você clicou no botão!');
+}
+function aoPerderFoco() {
+    alert('Você saiu do campo!');
+}
+
+// Selecionando o elemento usando querySelector
+let campo = document.querySelector('#meuCampo');
+let botao = document.querySelector('.btnCampo');
+
+// Adicionando os eventos
+campo.addEventListener('blur', aoPerderFoco);
+botao.addEventListener('click', aoClicar);

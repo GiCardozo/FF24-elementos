c56b2eccdfdf39a25cf0b56aef6c3c06019eb850
GIOVANNA CARDOZO CAETANO <giovanna.ccaetano@senacsp.edu.br>
2024-09-30 19:52:15 -0300

    Capturando elementos HTML

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Selecionando a div pelo ID
+let div =
+document.getElementById('meuDiv');
+
+// Selecionando todos os parágrafos com a classe "paragrafo"
+let paragrafos = 
+document.getElementsByClassName('paragrafo');
+
+// Selacionando o primeiro parágrafo com a classe "paragrafo"
+let primeiroParagrafo =
+document.querySelector('.paragrafo');
+
+// Selecionando todos os parágrafos dentro da div
+let paragrafoDentroDiv =
+document.querySelectorAll('#meuDiv p');
